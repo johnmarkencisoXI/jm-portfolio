d8e6cc8071ab6cdd9f3cbc66d9047b9b5121e4ea
John Mark Enciso <159507514+johnmarkencisoXI@users.noreply.github.com>
2024-02-11 00:09:12 +0800

    update pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
